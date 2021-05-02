638ef12c813fb1e6058a1793b418fa0a3d997704
TiagoManiero <tiago.maniero@totvs.com.br>
2021-05-02 18:44:17 -0300

    Primeiro commit api rest sala de reuniao

---
+++ src/main/java/com/tiagomaniero/saladereuniao/repository/RoomRepository.java
@@ -0,0 +1,9 @@
+package com.tiagomaniero.saladereuniao.repository;
+
+import com.tiagomaniero.saladereuniao.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+}
